32b91f35a817d12348d9cd0c3cb9ad77c703e462
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-12 01:36:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const btn=document.querySelector("#btn");
+const textInput=document.querySelector("#text");
+const delayInput=document.querySelector("#delay");
+const output=document.querySelector("#output");
+
+btn.onclick=handleAsyncAwait;
+
+ async function handleAsyncAwait() {
+	let promise1=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve(textInput.value);
+			},parseInt(delayInput.value));
+		});
+	let data=await promise1;
+	output.textContent=data;
+	textInput.value="";
+	delayInput.value="";	
+}
